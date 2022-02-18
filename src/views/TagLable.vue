9022e1816e52b293b264c4e90a2bab0669feee59
xiaocao <853044263@qq.com>
2022-02-19 02:20:46 +0800

    翻转箭头

--- src/views/TagLable.vue
+++ src/views/TagLable.vue
@@ -1,6 +1,9 @@
 <template>
   <Layout>
-    标签页
+    <div>
+      <Icon iconName="left"></Icon>
+      <span>标签页</span>
+    </div>
   </Layout>
 </template>
 
@@ -8,20 +11,19 @@
 import Vue from "vue";
 import {Component} from "vue-property-decorator";
 import models from "@/models/models";
+
 @Component
 export default class TagLable extends Vue {
-    created(){
-      const id = this.$route.params.id
-      models.getLanguage()
-      console.log(models.dataList)
-      const tag = models.dataList.filter(data => data.id === id)[0]
-      console.log(tag)
-      if(tag){
-        console.log(tag)
-      }else{
-        this.$router.replace('/404')
-      }
+  created() {
+    const id = this.$route.params.id;
+    models.getLanguage();
+    const tag = models.dataList.filter(data => data.id === id)[0];
+    if (tag) {
+      console.log(tag);
+    } else {
+      this.$router.replace('/404');
     }
+  }
 }
 </script>
 
